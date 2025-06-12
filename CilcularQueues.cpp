b89533aab30d3dea52588d012fa620b94db5baa6
TEJOCAHYO <tejo.cahyo.ft24@mail.umy.ac.id>
2025-06-12 11:25:45 +0700

    mendeklalasi posisi front dan rear

--- CilcularQueues.cpp
+++ CilcularQueues.cpp
@@ -75,12 +75,50 @@ public:
     void display()
     {
         int front_position = front;
-       
+        int rear_position = rear;
 
-int main()
-{
-    Queues q;
-    char ch;
+        // cek apakah antrian kosong
+        if (front == -1)
+        {
+            cout << "queue is empty\n";
+            return;
+        }
+
+        cout << "\nelements int the queue are... \n";
+
+        // jika front_position <= rear_position, literasi dari front hingga rear
+        if (front_position <= rear_position)
+        {
+            while (front_position <= rear_position)
+            {
+                cout << queue_array[front_position] << "  ";
+                front_position++;
+            }
+            cout << endl;
+        }
+        else
+        {
+            // jika front_position > rear_positionn, literasi dari front hingga akhir array
+            while (front_position <= max - 1)
+            {
+                cout << queue_array[front_position] << "  ";
+                front_position++;
+            }
+
+            front_position = 0;
+
+            // literasi dari awal array hingga rear
+            while (front_position <= rear_position)
+            {
+                cout << queue_array[front_position] << "  ";
+                front_position++;
+            }
+            cout << endl;
+        }
+    }
+};
+
+
 
     while (true)
     {
