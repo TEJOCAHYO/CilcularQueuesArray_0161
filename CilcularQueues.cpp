c65eaf0b4a747ab276ba9c37c9c0d06e0e7a4b9d
TEJOCAHYO <tejo.cahyo.ft24@mail.umy.ac.id>
2025-06-12 11:06:31 +0700

    memberikan class Queues

--- CilcularQueues.cpp
+++ CilcularQueues.cpp
@@ -1,22 +1,7 @@
 #include <iostream>
+using namespace std;
 
-    static const int max = 5;
-    int front, rear;
-    int queue_array[5];
 
-public:
-    Queues()
-    {
-        front = -1;
-        rear = -1;
-    }
-
-    void insert()
-    {
-        int num;
-        cout << "enter a number: ";
-        cin >> num;
-        cout << endl;
 
         // 1. cek apakah ada antrian penuh
         if ((front == 0 && rear == max - 1) || (front == rear + 1))
