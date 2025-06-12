3eabb0d9cb91e6a050937e147024ada28b154d3c
TEJOCAHYO <tejo.cahyo.ft24@mail.umy.ac.id>
2025-06-12 11:20:58 +0700

    memakai atribut bool;

--- CilcularQueues.cpp
+++ CilcularQueues.cpp
@@ -63,4 +63,109 @@ public:
             rear = -1;
         }
         else
-        
+        {
+            // jika elemen yang dihapus berada di posisi terakhir array, kembali ke awal array
+            if (front == max - 1)
+                front = 0;
+            else
+                front = front + 1;
+        }
+    }
+
+    void display()
+    {
+        int front_position = front;
+        int rear_position = rear;
+
+        // cek apakah antrian kosong
+        if (front == -1)
+        {
+            cout << "queue is empty\n";
+            return;
+        }
+
+        cout << "\nelements int the queue are... \n";
+
+        // jika front_position <= rear_position, literasi dari front hingga rear
+        if (front_position <= rear_position)
+        {
+            while (front_position <= rear_position)
+            {
+                cout << queue_array[front_position] << "  ";
+                front_position++;
+            }
+            cout << endl;
+        }
+        else
+        {
+            // jika front_position > rear_positionn, literasi dari front hingga akhir array
+            while (front_position <= max - 1)
+            {
+                cout << queue_array[front_position] << "  ";
+                front_position++;
+            }
+
+            front_position = 0;
+
+            // literasi dari awal array hingga rear
+            while (front_position <= rear_position)
+            {
+                cout << queue_array[front_position] << "  ";
+                front_position++;
+            }
+            cout << endl;
+        }
+    }
+};
+
+int main()
+{
+    Queues q;
+    char ch;
+
+    while (true)
+    {
+        try
+        {
+            cout << "menu" << endl;
+            cout << "1. implement insert operation" << endl;
+            cout << "2. implement delete operation" << endl;
+            cout << "3. display values" << endl;
+            cout << "4. exit" << endl;
+            cout << "enter your choice (1-4): ";
+            cin >> ch;
+            cout << endl;
+
+            switch (ch)
+            {
+            
+                q.insert();
+                break;
+            }
+            case '2':
+            {
+                q.remove();
+                break;
+            }
+            case '3':
+            {
+                q.display();
+                break;
+            }
+            case '4':
+            {
+                return 0;
+            }
+            default:
+            {
+                cout << "invalid option!!" << endl;
+                break;
+            }
+            }
+        }
+        catch (exception &e)
+        {
+            cout << "check for the values entered." << endl;
+        }
+    }
+}   
