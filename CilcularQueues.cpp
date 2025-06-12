b205363abc89368c83c9fa6db72b55fab2e89677
TEJOCAHYO <tejo.cahyo.ft24@mail.umy.ac.id>
2025-06-12 10:23:35 +0700

    memberikan library

---
+++ CilcularQueues.cpp
@@ -0,0 +1,61 @@
+
+
+class Queues
+{
+    private:
+    static const int max = 5;
+    int front, rear;
+    int queue_array[5];
+
+    public:
+    Queues()
+    {
+        front = -1;
+        rear = -1;
+    }
+
+    void insert()
+    {
+        int num;
+        cout << "enter a number: ";
+        cin >> num;
+        cout << endl;   
+
+        //1. cek apakah ada antrian penuh
+        if ((front == 0 && rear == max -1) || (front == rear + 1))
+        {
+            cout << "\nQueue overflow\n"; //1.a
+            return; //1.b
+        }
+
+        //2. cek apakah antrian kosong
+        if (front == -1)
+        {
+            front = 0; //2.a
+            rear = 0; //2.b
+        }
+        else
+        {
+            //jika rear berada di posisi terakgir array, kembali ke awal array
+            if (rear == max -1)
+                rear = 0;
+            else
+                rear = rear + 1; 
+        }
+        queue_array[rear] = num;
+    }
+
+    void remove()
+    {
+        //cek apakah antrian kosong
+        if (front == -1)
+        {
+            cout << "queue underflow\n";
+            return;
+        }
+    }
+
+};
+int main(){
+
+}
